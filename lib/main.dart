936c341d91a965eecbe618506a3e5933b01723d8
mogesT.23 <46963563+mogest23@users.noreply.github.com>
2022-09-16 14:11:28 +0300

    Fixes to styles.xml and increased minimum sdk value to 21

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_auth/src/enums.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_firebase_auth/src/home.screen.dart';
 import 'package:flutter_firebase_auth/src/providers/auth.providers.dart';
 import 'package:flutter_firebase_auth/src/routes/routes.dart';
 import 'package:flutter_firebase_auth/src/screens/auth.screen.dart';
-import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
